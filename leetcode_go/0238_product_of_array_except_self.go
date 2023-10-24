f595be71f2cb08ae3b3a0a8116b8bcc63a3e8c3f
丁浩天 <839338977@qq.com>
2023-10-24 23:24:45 +0800

    238. 除自身以外数组的乘积

---
+++ leetcode_go/0238_product_of_array_except_self.go
@@ -0,0 +1,21 @@
+package leetcode_go
+
+func productExceptSelf(nums []int) []int {
+	length := len(nums)
+	L, R, ans := make([]int, length), make([]int, length), make([]int, length)
+
+	L[0] = 1
+	for i := 1; i < length; i++ {
+		L[i] = nums[i-1] * L[i-1]
+	}
+	R[length-1] = 1
+	for i := length - 2; i >= 0; i-- {
+		R[i] = nums[i+1] * R[i+1]
+	}
+
+	// 对于索引 i，除 nums[i] 之外其余各元素的乘积就是左侧所有元素的乘积乘以右侧所有元素的乘积
+	for i := 0; i < length; i++ {
+		ans[i] = L[i] * R[i]
+	}
+	return ans
+}
